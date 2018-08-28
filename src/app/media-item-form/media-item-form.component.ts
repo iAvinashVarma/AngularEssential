7cda0ab57b2ac4eb32712fb442e939dd5bb9d15f
Avinash Varma <iavinashvarma@gmail.com>
2018-08-28 11:13:27 +0530

    Adding 5. Dependency Injection and Services (Dependency Injection => Class Constructor Injection).

--- src/app/media-item-form/media-item-form.component.ts
+++ src/app/media-item-form/media-item-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, AbstractControl, Validators } from '@angular/forms';
+import { FormGroup, AbstractControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-media-item-form',
@@ -8,17 +8,17 @@ import { FormGroup, FormControl, AbstractControl, Validators } from '@angular/fo
 })
 export class MediaItemFormComponent implements OnInit {
   mediaForm: FormGroup;
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.mediaForm = new FormGroup({
-      medium: new FormControl('Movies'),
-      name: new FormControl('', Validators.compose([
+    this.mediaForm = this.formBuilder.group({
+      medium: this.formBuilder.control('Movies'),
+      name: this.formBuilder.control('', Validators.compose([
         Validators.required,
         Validators.pattern('[\\w\\-\\s\\/]+')
       ])),
-      category: new FormControl('Action'),
-      year: new FormControl('', this.yearValidator)
+      category: this.formBuilder.control('Action'),
+      year: this.formBuilder.control('', this.yearValidator)
     });
   }
 
