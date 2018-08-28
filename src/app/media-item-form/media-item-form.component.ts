adbeaf5ba142b7a8c01adc6501a9dd657469ead8
Avinash Varma <iavinashvarma@gmail.com>
2018-08-28 10:50:45 +0530

    Updating settings.json and media item css.

--- src/app/media-item-form/media-item-form.component.ts
+++ src/app/media-item-form/media-item-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-media-item-form',
